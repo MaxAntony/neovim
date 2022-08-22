c231b0e28ba052f82c840b51dc26a7ce2cbd0c42
Max Antony <maxpacami@gmail.com>
2022-08-21 19:25:43 -0500

    added default options

---
+++ init.lua
@@ -0,0 +1,1 @@
+require'max.defaults'
---
+++ lua/max/defaults.lua
@@ -0,0 +1,42 @@
+local cmd = vim.cmd
+local o = vim.o
+local g = vim.g
+local opt = vim.opt
+
+-- Global settings
+cmd "set cursorline"
+cmd "set noshowmode"
+--cmd "set noshowcmd"
+cmd "set sw=2"
+cmd "set completeopt=menu,menuone,noselect"
+cmd "set tabstop=2"
+cmd "set shiftwidth=2"
+cmd "set expandtab"
+cmd "set smartindent"
+
+g.mapleader = ' '
+o.background = "dark"
+
+opt.timeoutlen = 300
+opt.termguicolors = true -- Enable colors in terminal
+opt.hlsearch = true --Set highlight on search
+opt.number = true --Make line numbers default
+opt.relativenumber = true --Make relative number default
+--opt.mouse = "a" --Enable mouse mode
+opt.breakindent = true --Enable break indent
+opt.undofile = true --Save undo history
+opt.ignorecase = true --Case insensitive searching unless /C or capital in search
+opt.smartcase = true -- Smart case
+opt.updatetime = 250 --Decrease update time
+opt.signcolumn = "yes" -- Always show sign column
+opt.clipboard = "unnamedplus" -- Access system clipboard
+
+-- Highlight on yank
+vim.cmd [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]]
+
+
