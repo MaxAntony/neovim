d345b1d290cb6e33ec8247d8ba57c007750d7e9a
Max Antony <maxpacami@gmail.com>
2022-08-21 20:25:35 -0500

    add: keymaps for telescope, floaterm and easy quit of terminal mode

--- lua/max/keymaps.lua
+++ lua/max/keymaps.lua
@@ -11,3 +11,13 @@ map("n", "<Leader>w", ":w<cr>")
 map('n', '<Leader>q', ':q<cr>')
 map("n", "<Leader>Q", ":wqa<cr>") -- quit nvim writing all
 map("n", "<Leader>e", ":NvimTreeToggle<cr>")
+-- floatTerm
+map('n', '<Leader>te', ':FloatermToggle<cr>')
+-- telescope
+map('n', '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_files()<cr>')
+map('n', '<leader>fg', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>')
+map('n', '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>')
+map('n', '<leader>fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>')
+
+-- TERMINAL
+map('t', '<ESC>', '<C-\\><C-n>')
