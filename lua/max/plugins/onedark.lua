82297b341cbd33a1c3f6b5c05a4ede205fee96ae
Max Antony <maxpacami@gmail.com>
2022-09-24 04:49:25 -0500

    remove white theme styles

--- lua/max/plugins/onedark.lua
+++ lua/max/plugins/onedark.lua
@@ -4,7 +4,7 @@ function M.setup()
     style = 'darker',
     -- toggle theme style ---
     toggle_style_key = '<leader>ts', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-    toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
+    toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' }, -- List of styles to toggle between
     code_style = {
       keywords = 'italic'
     }
