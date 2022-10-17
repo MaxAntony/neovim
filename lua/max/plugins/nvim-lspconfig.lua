19907471d3c9a293a5e6253d26894b0f66854288
Max Antony <maxpacami@gmail.com>
2022-10-17 14:03:05 -0500

    update: formateado y actualizado unas cosas

--- lua/max/plugins/nvim-lspconfig.lua
+++ lua/max/plugins/nvim-lspconfig.lua
@@ -1,215 +1,214 @@
 -- revisar https://github.com/neovim/nvim-lspconfig/wiki/Language-specific-plugins
 local M = {}
 function M.setup()
-	local languages = { "graphql", "prismals", "tailwindcss", "angularls", "cssls", "bashls", "dockerls" }
+  local languages = { "graphql", "prismals", "tailwindcss", "angularls", "cssls", "bashls", "dockerls" }
 
-	-- icons to the left bar
-	local signs = {
-		{ name = "DiagnosticSignError", text = " " },
-		{ name = "DiagnosticSignWarn", text = " " },
-		{ name = "DiagnosticSignInfo", text = " " },
-		{ name = "DiagnosticSignHint", text = " " },
-	}
+  -- icons to the left bar
+  local signs = {
+    { name = "DiagnosticSignError", text = " " },
+    { name = "DiagnosticSignWarn", text = " " },
+    { name = "DiagnosticSignInfo", text = " " },
+    { name = "DiagnosticSignHint", text = " " },
+  }
 
-	for _, sign in ipairs(signs) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
-	end
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+  end
 
-	-- Mappings.
-	-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-	local opts = { noremap = true, silent = true }
-	vim.keymap.set("n", "<Leader>m", vim.diagnostic.open_float, opts)
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-	-- comented by conflict  vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+  -- Mappings.
+  -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+  local opts = { noremap = true, silent = true }
+  vim.keymap.set("n", "<Leader>m", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+  -- comented by conflict  vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-	-- Use an on_attach function to only map the following keys
-	-- after the language server attaches to the current buffer
-	local on_attach = function(client, bufnr)
-		-- illuminate plugin config for highlight instead of underline
-		vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-		vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
-		vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+  -- Use an on_attach function to only map the following keys
+  -- after the language server attaches to the current buffer
+  local on_attach = function(client, bufnr)
+    -- illuminate plugin config for highlight instead of underline
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
 
-		-- Enable completion triggered by <c-x><c-o>
-		require("lsp-inlayhints").on_attach(client, bufnr)
-		vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-		-- Mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local bufopts = { noremap = true, silent = true, buffer = bufnr }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-		-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-		-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-		-- vim.keymap.set('n', '<space>wl', function()
-		-- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		-- end, bufopts)
-		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-		vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-		--[[ vim.keymap.set("n", "<space>f", function()
+    -- Enable completion triggered by <c-x><c-o>
+    require("lsp-inlayhints").on_attach(client, bufnr)
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    -- vim.keymap.set('n', '<space>wl', function()
+    -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    -- end, bufopts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+    --[[ vim.keymap.set("n", "<space>f", function()
 			vim.lsp.buf.format({ async = true })
 		end, bufopts) ]]
-	end
+  end
 
-	local lsp_flags = {
-		-- This is the default in Nvim 0.7+
-		debounce_text_changes = 150,
-	}
-	local lsp = require("lspconfig")
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	-- for ufo plugin
-	capabilities.textDocument.foldingRange = {
-		dynamicRegistration = false,
-		lineFoldingOnly = true,
-	}
-	-- end
-	capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
-	local luadev = require("lua-dev").setup({
-		lspconfig = {
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = {
-				Lua = {
-					hint = {
-						enable = true,
-						setType = true,
-						arrayIndex = "auto",
-						paramName = "all",
-						paramType = true,
-					},
-				},
-			},
-		},
-	})
+  local lsp_flags = {
+    -- This is the default in Nvim 0.7+
+    debounce_text_changes = 150,
+  }
+  local lsp = require("lspconfig")
+  -- for ufo plugin
 
-	lsp.sumneko_lua.setup(luadev)
+  -- end
+  local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  capabilities.textDocument.foldingRange = {
+    dynamicRegistration = false,
+    lineFoldingOnly = true,
+  }
 
-	for _, lang in ipairs(languages) do
-		-- for angular install @angular/language-server as dev dependency in your proyect
-		lsp[lang].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-	end
+  require("neodev").setup({})
 
-	lsp["pyright"].setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		flags = lsp_flags,
-	})
+  lsp.sumneko_lua.setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+      Lua = {
+        hint = {
+          enable = true,
+          setType = true,
+          arrayIndex = "auto",
+          paramName = "all",
+          paramType = true,
+        },
+      },
+    },
+  })
 
-	lsp["denols"].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-		root_dir = lsp.util.root_pattern("deno.json", "deno.jsonc"),
-		single_file_support = false,
-	})
+  for _, lang in ipairs(languages) do
+    -- for angular install @angular/language-server as dev dependency in your proyect
+    lsp[lang].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+  end
 
-	lsp["tsserver"].setup({
-		root_dir = lsp.util.root_pattern("package.json"),
-		capabilities = capabilities,
-		on_attach = on_attach,
-		flags = lsp_flags,
-		settings = {
-			javascript = {
-				inlayHints = {
-					includeInlayEnumMemberValueHints = true,
-					includeInlayFunctionLikeReturnTypeHints = true,
-					includeInlayFunctionParameterTypeHints = true,
-					includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-					includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-					includeInlayPropertyDeclarationTypeHints = true,
-					includeInlayVariableTypeHints = true,
-				},
-			},
-			typescript = {
-				inlayHints = {
-					includeInlayEnumMemberValueHints = true,
-					includeInlayFunctionLikeReturnTypeHints = true,
-					includeInlayFunctionParameterTypeHints = true,
-					includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-					includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-					includeInlayPropertyDeclarationTypeHints = true,
-					includeInlayVariableTypeHints = true,
-				},
-			},
-		},
-	})
+  lsp["pyright"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    flags = lsp_flags,
+  })
 
-	lsp["rust_analyzer"].setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		flags = lsp_flags,
-		-- Server-specific settings...
-		settings = {
-			["rust-analyzer"] = {},
-		},
-	})
+  lsp["denols"].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    root_dir = lsp.util.root_pattern("deno.json", "deno.jsonc"),
+    single_file_support = false,
+  })
 
-	lsp["jsonls"].setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		settings = {
-			json = {
-				-- Schemas https://www.schemastore.org
-				schemas = {
-					{
-						fileMatch = { "package.json" },
-						url = "https://json.schemastore.org/package.json",
-					},
-					{
-						fileMatch = { "tsconfig*.json" },
-						url = "https://json.schemastore.org/tsconfig.json",
-					},
-					{
-						fileMatch = { "deno.json", "deno.jsonc" },
-						url = "https://deno.land/x/deno@v1.25.2/cli/schemas/config-file.v1.json",
-					},
-					{
-						fileMatch = {
-							".prettierrc",
-							".prettierrc.json",
-							"prettier.config.json",
-						},
-						url = "https://json.schemastore.org/prettierrc.json",
-					},
-					{
-						fileMatch = { ".eslintrc", ".eslintrc.json" },
-						url = "https://json.schemastore.org/eslintrc.json",
-					},
-					{
-						fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
-						url = "https://json.schemastore.org/babelrc.json",
-					},
-					{
-						fileMatch = { "lerna.json" },
-						url = "https://json.schemastore.org/lerna.json",
-					},
-					{
-						fileMatch = { "now.json", "vercel.json" },
-						url = "https://json.schemastore.org/now.json",
-					},
-					{
-						fileMatch = {
-							".stylelintrc",
-							".stylelintrc.json",
-							"stylelint.config.json",
-						},
-						url = "http://json.schemastore.org/stylelintrc.json",
-					},
-					{
-						fileMatch = { ".cspell.json", "cspell.json" },
-						url = "https://raw.githubusercontent.com/streetsidesoftware/cspell/main/cspell.schema.json",
-					},
-				},
-			},
-		},
-	})
+  lsp["tsserver"].setup({
+    root_dir = lsp.util.root_pattern("package.json"),
+    capabilities = capabilities,
+    on_attach = on_attach,
+    flags = lsp_flags,
+    settings = {
+      javascript = {
+        inlayHints = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = true,
+        },
+      },
+      typescript = {
+        inlayHints = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = true,
+        },
+      },
+    },
+  })
+
+  lsp["rust_analyzer"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    flags = lsp_flags,
+    -- Server-specific settings...
+    settings = {
+      ["rust-analyzer"] = {},
+    },
+  })
+
+  lsp["jsonls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+      json = {
+        -- Schemas https://www.schemastore.org
+        schemas = {
+          {
+            fileMatch = { "package.json" },
+            url = "https://json.schemastore.org/package.json",
+          },
+          {
+            fileMatch = { "tsconfig*.json" },
+            url = "https://json.schemastore.org/tsconfig.json",
+          },
+          {
+            fileMatch = { "deno.json", "deno.jsonc" },
+            url = "https://deno.land/x/deno@v1.25.2/cli/schemas/config-file.v1.json",
+          },
+          {
+            fileMatch = {
+              ".prettierrc",
+              ".prettierrc.json",
+              "prettier.config.json",
+            },
+            url = "https://json.schemastore.org/prettierrc.json",
+          },
+          {
+            fileMatch = { ".eslintrc", ".eslintrc.json" },
+            url = "https://json.schemastore.org/eslintrc.json",
+          },
+          {
+            fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+            url = "https://json.schemastore.org/babelrc.json",
+          },
+          {
+            fileMatch = { "lerna.json" },
+            url = "https://json.schemastore.org/lerna.json",
+          },
+          {
+            fileMatch = { "now.json", "vercel.json" },
+            url = "https://json.schemastore.org/now.json",
+          },
+          {
+            fileMatch = {
+              ".stylelintrc",
+              ".stylelintrc.json",
+              "stylelint.config.json",
+            },
+            url = "http://json.schemastore.org/stylelintrc.json",
+          },
+          {
+            fileMatch = { ".cspell.json", "cspell.json" },
+            url = "https://raw.githubusercontent.com/streetsidesoftware/cspell/main/cspell.schema.json",
+          },
+        },
+      },
+    },
+  })
 end
 
 return M
