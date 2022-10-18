4005a7764f275f4515404fc6e30dbfcd56006230
Max Antony <maxpacami@gmail.com>
2022-10-18 00:27:55 -0500

    style: codigo formateado con reglas stylua

--- lua/max/plugins/onedark.lua
+++ lua/max/plugins/onedark.lua
@@ -1,15 +1,15 @@
 local M = {}
 function M.setup()
-  require("onedark").setup({
-    style = "darker",
-    -- toggle theme style ---
-    toggle_style_key = "<leader>ts", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-    toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer" }, -- List of styles to toggle between
-    code_style = {
-      keywords = "italic",
-    },
-  })
-  require("onedark").load()
+	require("onedark").setup({
+		style = "darker",
+		-- toggle theme style ---
+		toggle_style_key = "<leader>ts", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+		toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer" }, -- List of styles to toggle between
+		code_style = {
+			keywords = "italic",
+		},
+	})
+	require("onedark").load()
 end
 
 return M
