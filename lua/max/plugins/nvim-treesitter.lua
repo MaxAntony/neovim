2df5591f48adf2d5577bf303d52943889058c0e8
Max Antony <maxpacami@gmail.com>
2022-10-18 00:28:58 -0500

    refactor: se movio configuracion de tresitter a su propio archivo

---
+++ lua/max/plugins/nvim-treesitter.lua
@@ -0,0 +1,28 @@
+local M = {}
+function M.setup()
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = {
+			"tsx",
+			"html",
+			"lua",
+			"typescript",
+			"python",
+			"graphql",
+			"json",
+			"jsdoc",
+			"json5",
+			"markdown",
+			"prisma",
+			"javascript",
+		},
+		auto_install = true,
+		highlight = { enable = true },
+		rainbow = {
+			enable = true,
+			extended_mode = true,
+			max_file_lines = nil,
+		},
+	})
+end
+
+return M
