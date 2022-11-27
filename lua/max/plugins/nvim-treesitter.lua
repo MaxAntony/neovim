feb9a691d5c697d6488946b44d7b641d375c284c
Max Antony <maxpacami@gmail.com>
2022-11-26 22:41:03 -0500

    agregado nvim-ts-context-commentstring for comment jsx

--- lua/max/plugins/nvim-treesitter.lua
+++ lua/max/plugins/nvim-treesitter.lua
@@ -1,28 +1,32 @@
 local M = {}
 function M.setup()
-	require("nvim-treesitter.configs").setup({
-		ensure_installed = {
-			"tsx",
-			"html",
-			"lua",
-			"typescript",
-			"python",
-			"graphql",
-			"json",
-			"jsdoc",
-			"json5",
-			"markdown",
-			"prisma",
-			"javascript",
-		},
-		auto_install = true,
-		highlight = { enable = true },
-		rainbow = {
-			enable = true,
-			extended_mode = true,
-			max_file_lines = nil,
-		},
-	})
+  require("nvim-treesitter.configs").setup({
+    ensure_installed = {
+      "tsx",
+      "html",
+      "lua",
+      "typescript",
+      "python",
+      "graphql",
+      "json",
+      "jsdoc",
+      "json5",
+      "markdown",
+      "prisma",
+      "javascript",
+    },
+    auto_install = true,
+    highlight = { enable = true },
+    rainbow = {
+      enable = true,
+      extended_mode = true,
+      max_file_lines = nil,
+    },
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = false,
+    },
+  })
 end
 
 return M
