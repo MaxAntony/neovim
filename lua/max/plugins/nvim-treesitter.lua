499fb46e6e04308605915a5895fb8d85427c4a1c
MaxAntony <maxpacami@gmail.com>
2023-02-20 00:34:38 -0500

    remove autoinstall parsers from treesitter

--- lua/max/plugins/nvim-treesitter.lua
+++ lua/max/plugins/nvim-treesitter.lua
@@ -1,20 +1,20 @@
 local M = {}
 function M.setup()
   require('nvim-treesitter.configs').setup({
-    ensure_installed = {
-      'tsx',
-      'html',
-      'lua',
-      'typescript',
-      'python',
-      'graphql',
-      'json',
-      'jsdoc',
-      'json5',
-      'markdown',
-      'prisma',
-      'javascript',
-    },
+    -- ensure_installed = {
+    --   'tsx',
+    --   'html',
+    --   'lua',
+    --   'typescript',
+    --   'python',
+    --   'graphql',
+    --   'json',
+    --   'jsdoc',
+    --   'json5',
+    --   'markdown',
+    --   'prisma',
+    --   'javascript',
+    -- },
     auto_install = true,
     highlight = { enable = true },
     rainbow = {
