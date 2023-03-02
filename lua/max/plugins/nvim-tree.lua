d162bd5325801f0d035e5fcd94c29d2fca9230de
MaxAntony <maxpacami@gmail.com>
2023-03-02 18:45:18 -0500

    feat: update nvim-tree with default options

--- lua/max/plugins/nvim-tree.lua
+++ lua/max/plugins/nvim-tree.lua
@@ -1,8 +1,7 @@
 local M = {}
 function M.setup()
-  require('nvim-tree').setup({ -- BEGIN_DEFAULT_OPTS
+  require('nvim-tree').setup({
     auto_reload_on_write = true,
-    create_in_closed_folder = false,
     disable_netrw = false,
     hijack_cursor = false,
     hijack_netrw = true,
@@ -10,27 +9,26 @@ function M.setup()
     ignore_buffer_on_setup = false,
     open_on_setup = false,
     open_on_setup_file = false,
-    open_on_tab = false,
-    ignore_buf_on_tab_change = {},
     sort_by = 'name',
     root_dirs = {},
     prefer_startup_root = false,
     sync_root_with_cwd = false,
     reload_on_bufenter = false,
     respect_buf_cwd = false,
-    on_attach = 'disable', -- function(bufnr). If nil, will use the deprecated mapping strategy
-    remove_keymaps = false, -- boolean (disable totally or not) or list of key (lhs)
+    on_attach = 'default',
+    remove_keymaps = false,
+    select_prompts = false,
     view = {
-      adaptive_size = true,
       centralize_selection = false,
+      cursorline = true,
+      debounce_delay = 15,
       width = 30,
-      hide_root_folder = true,
+      hide_root_folder = false,
       side = 'left',
       preserve_window_proportions = false,
       number = false,
       relativenumber = false,
       signcolumn = 'yes',
-      -- @deprecated
       mappings = {
         custom_only = false,
         list = {
@@ -39,6 +37,7 @@ function M.setup()
       },
       float = {
         enable = false,
+        quit_on_focus_loss = true,
         open_win_config = {
           relative = 'editor',
           border = 'rounded',
@@ -55,7 +54,9 @@ function M.setup()
       highlight_git = false,
       full_name = false,
       highlight_opened_files = 'none',
-      root_folder_modifier = ':~',
+      highlight_modified = 'none',
+      root_folder_label = ':~:s?$?/..?',
+      indent_width = 2,
       indent_markers = {
         enable = false,
         inline_arrows = true,
@@ -63,12 +64,14 @@ function M.setup()
           corner = '└',
           edge = '│',
           item = '│',
+          bottom = '─',
           none = ' ',
         },
       },
       icons = {
         webdev_colors = true,
-        git_placement = 'after',
+        git_placement = 'before',
+        modified_placement = 'after',
         padding = ' ',
         symlink_arrow = ' ➛ ',
         show = {
@@ -76,11 +79,13 @@ function M.setup()
           folder = true,
           folder_arrow = true,
           git = true,
+          modified = true,
         },
         glyphs = {
           default = '',
           symlink = '',
           bookmark = '',
+          modified = '●',
           folder = {
             arrow_closed = '',
             arrow_open = '',
@@ -110,7 +115,7 @@ function M.setup()
       auto_open = true,
     },
     update_focused_file = {
-      enable = true,
+      enable = false,
       update_root = false,
       ignore_list = {},
     },
@@ -122,7 +127,12 @@ function M.setup()
     diagnostics = {
       enable = false,
       show_on_dirs = false,
+      show_on_open_dirs = true,
       debounce_delay = 50,
+      severity = {
+        min = vim.diagnostic.severity.HINT,
+        max = vim.diagnostic.severity.ERROR,
+      },
       icons = {
         hint = '',
         info = '',
@@ -131,19 +141,28 @@ function M.setup()
       },
     },
     filters = {
-      dotfiles = true,
-      custom = { '.git$' },
+      dotfiles = false,
+      git_clean = false,
+      no_buffer = false,
+      custom = {},
       exclude = {},
     },
     filesystem_watchers = {
       enable = true,
       debounce_delay = 50,
+      ignore_dirs = {},
     },
     git = {
       enable = true,
       ignore = true,
       show_on_dirs = true,
+      show_on_open_dirs = true,
       timeout = 400,
+    },
+    modified = {
+      enable = false,
+      show_on_dirs = true,
+      show_on_open_dirs = true,
     },
     actions = {
       use_system_clipboard = true,
@@ -166,10 +185,11 @@ function M.setup()
         },
       },
       open_file = {
-        quit_on_open = true,
+        quit_on_open = false,
         resize_window = true,
         window_picker = {
           enable = true,
+          picker = 'default',
           chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
           exclude = {
             filetype = { 'notify', 'packer', 'qf', 'diff', 'fugitive', 'fugitiveblame' },
@@ -183,11 +203,26 @@ function M.setup()
     },
     trash = {
       cmd = 'gio trash',
-      require_confirm = true,
     },
     live_filter = {
       prefix = '[FILTER]: ',
       always_show_folders = true,
+    },
+    tab = {
+      sync = {
+        open = false,
+        close = false,
+        ignore = {},
+      },
+    },
+    notify = {
+      threshold = vim.log.levels.INFO,
+    },
+    ui = {
+      confirm = {
+        remove = true,
+        trash = true,
+      },
     },
     log = {
       enable = false,
@@ -203,7 +238,7 @@ function M.setup()
         watcher = false,
       },
     },
-  }) -- END_DEFAULT_OPTS
+  })
 end
 
 return M
