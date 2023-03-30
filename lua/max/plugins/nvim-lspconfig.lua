e6288c9ea0a55d6b717a41285f4ad4e35512d3b9
MaxAntony <maxpacami@gmail.com>
2023-03-30 16:41:24 -0500

    feat: now go to definition uses telescope

--- lua/max/plugins/nvim-lspconfig.lua
+++ lua/max/plugins/nvim-lspconfig.lua
@@ -53,7 +53,9 @@ function M.setup()
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gd', ':Telescope lsp_definitions<cr>', bufopts)
+    -- the same as above but the window for select the correct definition closes after select
+    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
