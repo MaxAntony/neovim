2e23011d81c05f9e8ee8141c3cbd8d4069ac85f1
MaxAntony <maxpacami@gmail.com>
2023-06-12 17:17:50 -0500

    feat: word wrap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local o = vim.o
+local opt = vim.opt
 
-vim.o.foldcolumn = "1" -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+o.foldcolumn = "1" -- '0' is not bad
+o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+o.foldlevelstart = 99
+o.foldenable = true
+o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+-- vim.opt.breakindent = true
+opt.wrap = true
+opt.linebreak = true
