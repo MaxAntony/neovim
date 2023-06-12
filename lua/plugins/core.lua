9e33287d7452680562256d566284cd6925eb3d32
MaxAntony <maxpacami@gmail.com>
2023-06-12 12:28:05 -0500

    feat: neo-tree close on open file event

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -14,4 +14,18 @@ return {
       },
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            --auto close
+            require("neo-tree").close_all()
+          end,
+        },
+      },
+    },
+  },
 }
