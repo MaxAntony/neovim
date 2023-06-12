126e790eb3294ff084ba41fbf09c97d58d6de150
MaxAntony <maxpacami@gmail.com>
2023-06-12 12:11:56 -0500

    feat: add and configure nvim-ufo plugin for folding

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,22 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      capabilities={
+        textDocument = {
+          foldingRange = {
+            -- UFO folding
+            dynamicRegistration = false,
+            lineFoldingOnly = true,
+          },
+        },
+      }
+    },
+  }
+} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
