d3a47b839c50187d40373dcd23100e093498e35d
MaxAntony <maxpacami@gmail.com>
2023-07-27 10:50:42 -0500

    :sparkles: New shortcut for telescope buffers
    list buffers in opened order and omit current

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -128,4 +128,14 @@ return {
     },
   },
   { 'folke/tokyonight.nvim', opts = { style = 'storm' } },
+  {
+    'nvim-telescope/telescope.nvim',
+    keys = {
+      {
+        '<leader>fB',
+        "<cmd>lua require('telescope.builtin').buffers({ sort_mru = true, ignore_current_buffer = true })<cr>",
+        desc = 'last buffers',
+      },
+    },
+  },
 }
