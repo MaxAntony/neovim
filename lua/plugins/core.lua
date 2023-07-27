74af4d6bd58e53edb9437bb9180b3e598ac1c26f
MaxAntony <maxpacami@gmail.com>
2023-07-27 10:45:47 -0500

    :wastebasket: Remove icon fix for neo-tree
    neo-tree fixes nerd fonts icons by itself

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -125,47 +125,6 @@ return {
           end,
         },
       },
-      -- remove when neo-tree update to nerdfonts v3
-      -- for detect obsolete font icons you can use Nerdfix https://github.com/loichyan/nerdfix
-      default_component_configs = {
-        icon = {
-          folder_empty = '󰜌',
-          folder_empty_open = '󰜌',
-        },
-        git_status = {
-          symbols = {
-            renamed = '󰁕',
-            unstaged = '󰄱',
-          },
-        },
-      },
-      document_symbols = {
-        kinds = {
-          File = { icon = '󰈙', hl = 'Tag' },
-          Namespace = { icon = '󰌗', hl = 'Include' },
-          Package = { icon = '󰏖', hl = 'Label' },
-          Class = { icon = '󰌗', hl = 'Include' },
-          Property = { icon = '󰆧', hl = '@property' },
-          Enum = { icon = '󰒻', hl = '@number' },
-          Function = { icon = '󰊕', hl = 'Function' },
-          String = { icon = '󰀬', hl = 'String' },
-          Number = { icon = '󰎠', hl = 'Number' },
-          Array = { icon = '󰅪', hl = 'Type' },
-          Object = { icon = '󰅩', hl = 'Type' },
-          Key = { icon = '󰌋', hl = '' },
-          Struct = { icon = '󰌗', hl = 'Type' },
-          Operator = { icon = '󰆕', hl = 'Operator' },
-          TypeParameter = { icon = '󰊄', hl = 'Type' },
-          StaticMethod = { icon = '󰠄 ', hl = 'Function' },
-        },
-      },
-      -- Add this section only if you've configured source selector.
-      source_selector = {
-        sources = {
-          { source = 'filesystem', display_name = ' 󰉓 Files ' },
-          { source = 'git_status', display_name = ' 󰊢 Git ' },
-        },
-      },
     },
   },
   { 'folke/tokyonight.nvim', opts = { style = 'storm' } },
