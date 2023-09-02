c5e85f5a683d1a51b9a3110a062d0175522bde0f
MaxAntony <maxpacami@gmail.com>
2023-09-02 18:09:38 -0500

    :sparkles: Feat: disable resize buffer animation and set bordered popup for cmp

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -138,4 +138,20 @@ return {
       },
     },
   },
+  {
+    'echasnovski/mini.animate',
+    opts = { resize = { enable = false } },
+  },
+  -- override nvim-cmp
+  {
+    'hrsh7th/nvim-cmp',
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require('cmp')
+      opts.window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
+    end,
+  },
 }
