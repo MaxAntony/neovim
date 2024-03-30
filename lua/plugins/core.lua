1e4d3aa13ee8f7cfcadb4602b78982a8d2445049
MaxAntony <maxpacami@gmail.com>
2024-03-30 10:38:58 -0500

    :bulb: Chore: add comment

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -168,6 +168,7 @@ return {
     opts = function(_, opts)
       local cmp = require('cmp')
       opts.window = {
+        -- https://github.com/LazyVim/LazyVim/issues/556
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       }
